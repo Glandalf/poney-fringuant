d0952ce3c37f26ef899b1d021ccceb4e1d23d3fd
Renaud <renaud.angles@asciiparait.fr>
2021-02-04 10:18:15 +0100

    ajoute un formulaire d'inscription HTML/JS
    On a un fichier enregistrement.php qui renvoie des données factices.

--- backend/enregistrement.php
+++ backend/enregistrement.php
@@ -4,10 +4,23 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
+include('bdd.php');
+include('headers.php');
 /* 
     On peut vérifier que les variables attendues 
     dans la doc sont bien envoyées par le front :
 */
-echo $_POST['prenom'];
-echo $_POST['pseudo'];
-echo $_POST['numero'];
+    
+// echo $_POST['id'] . '\n';
+// echo $_POST['prenom'] . '\n';
+// echo $_POST['nom'] . '\n';
+// echo $_POST['pseudo'] . '\n';
+// echo $_POST['password'] . '\n';
+// echo $_POST['email'] . '\n';
+// echo $_POST['numero'] . '\n';
+// echo $_POST['adresse1'] . '\n';
+// echo $_POST['codePostal'] . '\n';
+// echo $_POST['ville'] . '\n';
+// echo $_POST['dateAdhesion'] . '\n';
+
+echo "{'status': 'bof', 'description': 'cette fonctionnalité n\'est pas encore codée !!'}";
--- frontend/js/index.js
+++ frontend/js/index.js
@@ -1,5 +1,5 @@
 // Paramétrage de notre appli front 
-const backendRootURL = 'http://localhost:8000/';
+const backendRootURL = 'http://localhost:8001/';
 
 // Une fois la page HTML chargée, 
 // on veut surcharger le submit du formulaire
@@ -7,23 +7,21 @@ const backendRootURL = 'http://localhost:8000/';
 postInscriptionAJAX();
 
 function postInscriptionAJAX() {
-    console.log(document.querySelector('#inscription'))
     document.querySelector('#inscription').addEventListener('submit', (ev) => {
+        const form = document.querySelector('#inscription'); 
         // On ne veut pas que le navigateur fasse le post lui même et quitte la page
         ev.preventDefault();
 
         // On fabrique notre "form data", les champs de form à envoyer :
         const data = new URLSearchParams();
-        for (const pair of new FormData(document.querySelector('#inscription'))) {
+        for (const pair of new FormData(form)) {
             data.append(pair[0], pair[1]);
         }
 
-        // TODO: gérer le CORS
-        // TODO: vérifier que le fomulaire arrive bien côté php
         fetch(backendRootURL + 'enregistrement.php', {
-                    method: 'POST',
-                    body: data
-                })
+            method: 'POST',
+            body: data
+        })
             .then(response => response.json())
             .then(data => console.log('INSCRIPTION REUSSIE', data))
             .catch(error => console.error('INSCRIPTION ECHOUEE', error))
